806d38eab6552c9a2570b05987ef8ff1f57ef348
Joel Cabatay <ascii274@gmail.com>
2022-02-16 22:18:27 +0100

    Initial commit

---
+++ RetoMWC-2022/src/main/java/com/ascii274/retomwc2022/RetoMwc2022Application.java
@@ -0,0 +1,13 @@
+package com.ascii274.retomwc2022;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetoMwc2022Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetoMwc2022Application.class, args);
+	}
+
+}
