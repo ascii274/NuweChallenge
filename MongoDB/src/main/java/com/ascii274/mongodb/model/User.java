d43bd3480b9a07622909d5b31acb88d440b20a09
Joel Cabatay <ascii274@gmail.com>
2022-04-04 10:52:07 +0200

    User entity created

---
+++ MongoDB/src/main/java/com/ascii274/mongodb/model/User.java
@@ -0,0 +1,65 @@
+package com.ascii274.mongodb.model;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+
+@Getter
+@Setter
+@Data
+@Document(collection = "users")
+public class User {
+    @Id
+    @Field(name="id")
+    private Long id;
+    @Field(name="username")
+    private String username;
+    @Indexed(unique = true)
+    @Field(name="age")
+    private int age;
+    @Field(name="city")
+    private String city;
+
+    public User(String username, int age, String city) {
+        this.username = username;
+        this.age = age;
+        this.city = city;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "username='" + username + '\'' +
+                ", age=" + age +
+                ", city='" + city + '\'' +
+                '}';
+    }
+}
